dae75f098feeba096d9eb88315a6c603644e5b32
Leedasom <somdaya@naver.com>
2023-08-16 21:16:13 +0900

    fix: return값 변경, if문 제거

--- Search_Activity.py
+++ Search_Activity.py
@@ -3,12 +3,10 @@
 from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import requests
 
 # 한번에 10개씩 가져오고, 다음 10개 가져올려면 start_index=10, 그 다음 10개 가져올려면 start_index=20 ---
-def Search_Activity(start_index=0) : # 대외활동 검색 함수
+def Search_Activity(keyword='',start_index=0) : # 대외활동 검색 함수
     
-    keyword = requests.args.get('keword') # keyword값을 파라미터로 입력받음
     baseUrl = 'https://www.campuspick.com/activity?keyword=' # 캠퍼스픽 링크
     plusUrl = quote_plus(keyword) # 검색어 링크
     url = baseUrl + plusUrl # 전체 링크
@@ -23,11 +21,6 @@ def Search_Activity(start_index=0) : # 대외활동 검색 함수
     activity_list = soup.select('a.top') # 검색결과가 제목, 링크 담고있는 요소 선택
     
     result_list = [] # 크롤링 결과 담을 리스트
-    
-    # 크롤링 결과 없으면 종료(빈 리스트 반환)
-    if len(result_list) == 0 :
-        driver.close()
-        return result_list
     
     # 시작 인덱스가 리스트 범위를 벗어나면 함수 종료
     if start_index >= len(activity_list) :
@@ -54,4 +47,4 @@ def Search_Activity(start_index=0) : # 대외활동 검색 함수
         result_list.append(activity_info)
         
     driver.close() # 드라이버 닫기
-    return activity_info # 함수값 반환
+    return result_list # 함수값 반환
