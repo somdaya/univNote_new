c766af9f1d36d2dad006b97f415de65edcadf14e
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-08-20 12:13:08 +0900

    feat: 공모전/대외활동/동아리 더보기 API 구현

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+from Plus_Activity import plus_activity
+from Plus_Club import plus_club
+from Plus_Contest import plus_contest
 
 from Search_Activity import search_activity
 from Search_Club import search_club
@@ -22,6 +25,24 @@ def search_activities(keyword):
 def search_clubs(keyword):
     idx = request.args.get('idx', default=0, type=int)
     data = search_club(keyword=keyword, start_index=idx)
+    return data;
+
+@app.route("/contest", methods=['GET'])
+def show_all_contest():
+    idx = request.args.get('idx', default=0, type=int)
+    data = plus_contest(start_index=idx)
+    return data;
+
+@app.route("/activity", methods=['GET'])
+def show_all_activity():
+    idx = request.args.get('idx', default=0, type=int)
+    data = plus_activity(start_index=idx)
+    return data;
+
+@app.route("/club", methods=['GET'])
+def show_all_club():
+    idx = request.args.get('idx', default=0, type=int)
+    data = plus_club(start_index=idx)
     return data;
 
 if __name__ == "__main__":
