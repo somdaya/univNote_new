8b75a28b7bf1d6a08f50ca709fa58caa4a27b96e
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-08-20 12:06:00 +0900

    feat: 함수명 수정

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 from flask import Flask, request
-from Plus_Activity import Plus_Activity
-from Search_Activity import Search_Activity
-from Search_Club import Search_Club
 
-from Search_Contest import Search_Contest
+from Search_Activity import search_activity
+from Search_Club import search_club
+from Search_Contest import search_contest
 
 app = Flask(__name__)
 
 @app.route("/contest/search/<keyword>", methods=['GET'])
-def search_contest(keyword):
+def search_contents(keyword):
     idx = request.args.get('idx', default=0, type=int)
-    data = Search_Contest(keyword=keyword, start_index=idx)
+    data = search_contest(keyword=keyword, start_index=idx)
     return data;
   
 @app.route("/activity/search/<keyword>", methods=['GET'])
-def search_activity(keyword):
+def search_activities(keyword):
   idx = request.args.get('idx', default=0, type=int)
-  data = Search_Activity(keyword=keyword, start_index=idx)
+  data = search_activity(keyword=keyword, start_index=idx)
   return data;
 
 @app.route("/club/search/<keyword>", methods=['GET'])
-def search_club(keyword):
+def search_clubs(keyword):
     idx = request.args.get('idx', default=0, type=int)
-    data = Search_Club(keyword=keyword, start_index=idx)
+    data = search_club(keyword=keyword, start_index=idx)
     return data;
 
 if __name__ == "__main__":
